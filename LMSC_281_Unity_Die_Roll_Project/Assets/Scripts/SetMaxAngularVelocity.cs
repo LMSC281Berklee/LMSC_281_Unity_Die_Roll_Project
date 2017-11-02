2f938e93709e2518471a89853168227d26c17af1
DuncanAnderson <32934485+DuncanAnderson@users.noreply.github.com>
2017-11-01 20:11:23 -0400

    Update SetMaxAngularVelocity.cs

--- LMSC_281_Unity_Die_Roll_Project/Assets/Scripts/SetMaxAngularVelocity.cs
+++ LMSC_281_Unity_Die_Roll_Project/Assets/Scripts/SetMaxAngularVelocity.cs
@@ -5,11 +5,12 @@ using System.Collections;
 
 public class SetMaxAngularVelocity : MonoBehaviour
 {
-	public float maxAngularVelocity = 7.0f;
+	public float maxAngularVelocity = 7.0f; // variable with a value for the limit of rotation
 
 	// Use this for initialization
 	void Start ()
 	{
+		// this sets a limit to the rotation of the die so it doesnt spin out of control
 		GetComponent<Rigidbody>().maxAngularVelocity = maxAngularVelocity;
 	}
 }
