f7de9de405330a238d7f1272d7f480034c1c7dba
Peter <pxiong3@outlook.com>
2017-11-02 00:21:59 -0400

    Add files via upload
    Added notes for the scripts

---
+++ SetMaxAngularVelocity.cs
@@ -0,0 +1,18 @@
+﻿//example provided by http://www.cookingwithunity.com/
+
+using UnityEngine;
+using System.Collections;
+
+public class SetMaxAngularVelocity : MonoBehaviour
+{
+	public float maxAngularVelocity = 7.0f;
+    //When this is initated in game, this causes the die to move in an angle
+
+	// Use this for initialization
+	void Start ()
+	{
+		GetComponent<Rigidbody>().maxAngularVelocity = maxAngularVelocity;
+        //Takes the RigidBody component and applies angular velocity
+	}
+}
+//This is also attachedd directly to the die
