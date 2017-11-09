27cdb958f86dd799c64da308df8812d063600309
SimonG <sgrefenstette@berk>
2017-11-09 10:12:18 -0500

    Working Fire Button
    fire once

--- LMSC_281_Unity_Die_Roll_Project/Assets/Scripts/ApplyForceInRandomDirection.cs
+++ LMSC_281_Unity_Die_Roll_Project/Assets/Scripts/ApplyForceInRandomDirection.cs
@@ -5,7 +5,7 @@ using System.Collections;
 
 public class ApplyForceInRandomDirection : MonoBehaviour
 {
-	public string buttonName = "Fire1";
+	public string buttonName = "r";
 	public float forceAmount = 10.0f;
 	public float torqueAmount = 10.0f;
 	public ForceMode forceMode;
@@ -15,7 +15,10 @@ public class ApplyForceInRandomDirection : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-//		if(Input.GetButtonDown(buttonName))
+		if(Input.GetButtonDown(buttonName))
+		{
+			automateRoll = true;
+		}
 
 		if (automateRoll)
 		{
