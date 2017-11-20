4b30459caa6422d522c9951a3c17420812621157
lawk88 <lawrence.delloye@gmail.com>
2017-11-20 16:29:35 -0500

    Autoroll with bool

--- LMSC_281_Unity_Die_Roll_Project/Assets/Scripts/ApplyForceInRandomDirection.cs
+++ LMSC_281_Unity_Die_Roll_Project/Assets/Scripts/ApplyForceInRandomDirection.cs
@@ -8,15 +8,27 @@ public class ApplyForceInRandomDirection : MonoBehaviour
 	public string buttonName = "Fire1";
 	public float forceAmount = 10.0f;
 	public float torqueAmount = 10.0f;
+    public int numOfRolls = 100;
+    public int[] valOfRolls = new int[100]; 
+    public bool automateRoll;
 	public ForceMode forceMode;
 
-	// Update is called once per frame
-	void Update ()
+    void Start()
+    {
+        automateRoll = false;    
+    }
+    // Update is called once per frame
+    void Update ()
 	{
-		if(Input.GetButtonDown(buttonName))
-		{
-			GetComponent<Rigidbody>().AddForce(Random.onUnitSphere*forceAmount,forceMode);
-			GetComponent<Rigidbody>().AddTorque(Random.onUnitSphere*torqueAmount,forceMode);
-		}
+        if (Input.GetButtonDown(buttonName))
+        {
+            while (numOfRolls > 0)
+            {
+                GetComponent<Rigidbody>().AddForce(Random.onUnitSphere * forceAmount, forceMode);
+                GetComponent<Rigidbody>().AddTorque(Random.onUnitSphere * torqueAmount, forceMode);
+                Debug.Log("1 roll down");
+                numOfRolls--;
+            }
+        }
 	}
 }
