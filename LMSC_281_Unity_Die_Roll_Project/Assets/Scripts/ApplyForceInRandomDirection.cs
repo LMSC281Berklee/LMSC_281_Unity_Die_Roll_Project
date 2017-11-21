37b00427a01aa1e1544a58653c413ffe0ccc1f8d
lawk88 <lawrence.delloye@gmail.com>
2017-11-20 21:44:37 -0500

    17Fall_LDelloye
    Restart Button.
    Array Works.
    Automation works.

--- LMSC_281_Unity_Die_Roll_Project/Assets/Scripts/ApplyForceInRandomDirection.cs
+++ LMSC_281_Unity_Die_Roll_Project/Assets/Scripts/ApplyForceInRandomDirection.cs
@@ -1,34 +1,36 @@
-﻿//example provided by http://www.cookingwithunity.com/
+﻿//Lawrence Delloye
+//LMSC 281
+//Fall 2017
+
+//example provided by http://www.cookingwithunity.com/
 
 using UnityEngine;
 using System.Collections;
 
 public class ApplyForceInRandomDirection : MonoBehaviour
 {
-	public string buttonName = "Fire1";
-	public float forceAmount = 10.0f;
-	public float torqueAmount = 10.0f;
-    public int numOfRolls = 100;
-    public int[] valOfRolls = new int[100]; 
+    //Variables
+    //public string buttonName = "Fire1";
+
+    public float forceAmount = 10.0f;
+    public float torqueAmount = 10.0f;
     public bool automateRoll;
 	public ForceMode forceMode;
+    //Speed up Die Automation
+    public float timeSpeed = 5.0F;
 
     void Start()
     {
-        automateRoll = false;    
+        automateRoll = false;
+        Time.timeScale = timeSpeed;
     }
     // Update is called once per frame
     void Update ()
 	{
-        if (Input.GetButtonDown(buttonName))
+        if (automateRoll)
         {
-            while (numOfRolls > 0)
-            {
-                GetComponent<Rigidbody>().AddForce(Random.onUnitSphere * forceAmount, forceMode);
-                GetComponent<Rigidbody>().AddTorque(Random.onUnitSphere * torqueAmount, forceMode);
-                Debug.Log("1 roll down");
-                numOfRolls--;
-            }
+             GetComponent<Rigidbody>().AddForce(Random.onUnitSphere * forceAmount, forceMode);
+             GetComponent<Rigidbody>().AddTorque(Random.onUnitSphere * torqueAmount, forceMode);
         }
 	}
 }
---
+++ LMSC_281_Unity_Die_Roll_Project/Assets/Scripts/Restart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    public void RestartButton()
+    {
+        //This will load the active scene.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
